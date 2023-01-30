8ba343bf392b22f2925804f2de9c554598186afe
lauren-mac <109279133+lauren-mac@users.noreply.github.com>
2023-01-30 17:03:25 +0000

    fixed js file so current day displayed in header

--- starter/script.js
+++ starter/script.js
@@ -1,21 +1,14 @@
-// Enable js dynamic events to be added by the user
-// click event function, var for time - plant flag, var for event - free text 
-$(document).ready(function() {
-    // adjustment to get events from local storage when page loads
-    $('.timeblock-row').each(function() {
-      var time = $(this).find('.time').text();
-      var storedEvent = localStorage.getItem(time);
-      if (storedEvent) {
-        $(this).find('.event').text(storedEvent);
-      }
-    });
+// JUMBOTRON
+// Display the current day at the top of the calendar when a user opens the planner.
+// retrieve current date, add to header id currentDay
 
-    $('.timeblock-row').click(function() {
-      var time = $(this).find('.time').text();
-      var event = prompt("Add event for " + time);
-      $(this).find('.event').text(event);
-      // adjustment to add new event to local storage
-      localStorage.setItem(time, event);
-    });
-  });
+var dateHeader = document.querySelector("#currentDay");
+var today = new Date();
+dateHeader.innerHTML = today.toDateString();
+
+// var dateHeader = document.querySelector("#currentDay");
+// var today = moment();
+// dateHeader.innerHTML = today.format('MMMM Do YYYY');
+
+// why doesn't moment.js work???
 
